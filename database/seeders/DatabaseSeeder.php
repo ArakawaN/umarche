4483154b692c8f873c4fafe082ca636f4acf0c3c
ArakawaN <arkw1155@gmail.com>
2023-10-08 23:27:16 +0900

    product&stock_seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
+use App\Models\Stock;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -28,5 +31,8 @@ class DatabaseSeeder extends Seeder
             // StockSeeder::class,
             UserSeeder::class,
         ]);
+
+        Product::factory()->count(100)->create();
+        Stock::factory()->count(100)->create();
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\Product;
+
 
 class ProductSeeder extends Seeder
 {
@@ -45,5 +47,6 @@ class ProductSeeder extends Seeder
             ],
 
         ]);
+        Product::factory()->count(100)->create();
     }
 }
